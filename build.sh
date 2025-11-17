2c7d61518c55cb78617c986a2a6420ececbd9e3b
mikhail.karbat <mikhail.karbat@proton.me>
2025-11-17 22:28:17 +0300

    добавлен скрит для локальной сборки

---
+++ build.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+cd ..
+BASE_DIR=$(pwd)
+
+# Пути к директориям
+VENV_DIR="$BASE_DIR/keyboard_engines/venv_zmk/bin"
+ZMK_DIR="$BASE_DIR/keyboard_engines/zmk"
+CONFIG_DIR="$BASE_DIR/sofle-custom-rus-zmk"
+BUILD_DIR="$CONFIG_DIR/_builds"
+
+# Создаем папку для сборок если не существует
+mkdir -p "$BUILD_DIR"
+
+cd $VENV_DIR
+. ./activate
+
+# Переходим в директорию zmk
+cd $ZMK_DIR
+
+# Очистка предыдущих сборок
+rm -rf build
+
+# Сборка левой половины
+echo "Building left side..."
+west build -s app -b nice_nano_v2 -- -DSHIELD=sofle_left -DZMK_CONFIG="$CONFIG_DIR/config"
+mv $ZMK_DIR/build/zephyr/zmk.uf2 $BUILD_DIR/sofle_left.uf2
+
+# Очистка перед сборкой правой половины
+rm -rf build
+
+# Сборка правой половины (исправлен путь конфига)
+echo "Building right side..."
+west build -s app -b nice_nano_v2 -- -DSHIELD=sofle_right -DZMK_CONFIG="$CONFIG_DIR/config"
+mv $ZMK_DIR/build/zephyr/zmk.uf2 $BUILD_DIR/sofle_right.uf2
+
+echo "Build complete!"
